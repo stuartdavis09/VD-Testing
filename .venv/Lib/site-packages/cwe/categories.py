bdcffe38395b3d68c1093ac993f1afcdc97eb539
stuartdavis09 <stuartdavist@gmail.com>
2024-10-09 13:47:37 +0700

    gitignore

--- .venv/Lib/site-packages/cwe/categories.py
+++
@@ -1,9 +0,0 @@
-import enum
-
-
-@enum.unique
-class CWECategory(enum.Enum):
-
-    HARDWARE_DESIGN: str = "hardware_design"
-    RESEARCH_CONCEPTS: str = "research_concepts"
-    SOFTWARE_DEVELOPMENT: str = "software_development"
