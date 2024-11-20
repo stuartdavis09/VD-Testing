1c43417c748a40f32ae2943a8d2c5828d268857f
stuartdavis09 <stuartdavist@gmail.com>
2024-11-20 16:56:09 +0700

    Update groqVulnTesting.py

--- groqVulnTesting.py
+++ groqVulnTesting.py
@@ -191,42 +191,42 @@ def chatBot():
     ############################################################
 
 
-    # chat_completion = client.chat.completions.create(
-    #     messages = [
-    #         {
-    #             "role" : "system",
-    #             "content" : prompts[inputs["promptingTechnique"]],
-    #         },
-    #         {
-    #             "role" : "user",
-    #             "content" : promptTemplate,
-    #         },
-    #     ],
+    chat_completion = client.chat.completions.create(
+        messages = [
+            {
+                "role" : "system",
+                "content" : prompts[inputs["promptingTechnique"]],
+            },
+            {
+                "role" : "user",
+                "content" : promptTemplate,
+            },
+        ],
 
-    #     model=inputs["model"],
-    #     temperature = 0.5
-    # )
+        model=inputs["model"],
+        temperature = 0.5
+    )
 
-    # if (inputs["promptingTechnique"] == "SD1" or inputs["promptingTechnique"] == "SD2" or inputs["promptingTechnique"] == "SD3" or inputs["promptingTechnique"] == "SD4" or inputs["promptingTechnique"] == "SD5"):
-    #     dataStore = open("{0}\Results\{1}\CWE-2023-{2}-{3}-{4}-testing.txt".format(inputs["model"], inputs["testingDataset"], inputs["testingCodeNumber"], inputs["promptingTechnique"], inputs["patchStatus"]), "x")
-    #     dataStore.write(chat_completion.choices[0].message.content)
-
-
+    if (inputs["promptingTechnique"] == "SD1" or inputs["promptingTechnique"] == "SD2" or inputs["promptingTechnique"] == "SD3" or inputs["promptingTechnique"] == "SD4" or inputs["promptingTechnique"] == "SD5"):
+        dataStore = open("{0}\Results\{1}\CWE-2023-{2}-{3}-{4}-testing.txt".format(inputs["model"], inputs["testingDataset"], inputs["testingCodeNumber"], inputs["promptingTechnique"], inputs["patchStatus"]), "x")
+        dataStore.write(chat_completion.choices[0].message.content)
 
 
-    # elif inputs["promptingTechnique"] == "D1" or inputs["promptingTechnique"] == "D2" or inputs["promptingTechnique"] == "D3" or inputs["promptingTechnique"] == "D4" or inputs["promptingTechnique"] == "D5":
-    #     dataStore = open("{0}\ResultsSecLLMHolmesPrompts\{1}\CWE-2023-{2}-{3}-{4}-testing.txt".format(inputs["model"], inputs["testingDataset"], inputs["testingCodeNumber"], inputs["promptingTechnique"], inputs["patchStatus"]), "x")
-    #     dataStore.write(chat_completion.choices[0].message.content)
 
 
-    # elif inputs["promptingTechnique"] == "cwe-df":
-    #     dataStore = open("{0}\ResultsCWE-df\{1}\CWE-2023-{2}-{3}-{4}-testing.txt".format(inputs["model"], inputs["testingDataset"], inputs["testingCodeNumber"], inputs["promptingTechnique"], inputs["patchStatus"]), "x")
-    #     dataStore.write(chat_completion.choices[0].message.content)
+    elif inputs["promptingTechnique"] == "D1" or inputs["promptingTechnique"] == "D2" or inputs["promptingTechnique"] == "D3" or inputs["promptingTechnique"] == "D4" or inputs["promptingTechnique"] == "D5":
+        dataStore = open("{0}\ResultsSecLLMHolmesPrompts\{1}\CWE-2023-{2}-{3}-{4}-testing.txt".format(inputs["model"], inputs["testingDataset"], inputs["testingCodeNumber"], inputs["promptingTechnique"], inputs["patchStatus"]), "x")
+        dataStore.write(chat_completion.choices[0].message.content)
 
 
-    # elif inputs["promptingTechnique"] == "OP":
-    #     dataStore = open("{0}\ResultsOP\{1}\CWE-2023-{2}-{3}-{4}-testing.txt".format(inputs["model"], inputs["testingDataset"], inputs["testingCodeNumber"], inputs["promptingTechnique"], inputs["patchStatus"]), "x")
-    #     dataStore.write(chat_completion.choices[0].message.content)
+    elif inputs["promptingTechnique"] == "cwe-df":
+        dataStore = open("{0}\ResultsCWE-df\{1}\CWE-2023-{2}-{3}-{4}-testing.txt".format(inputs["model"], inputs["testingDataset"], inputs["testingCodeNumber"], inputs["promptingTechnique"], inputs["patchStatus"]), "x")
+        dataStore.write(chat_completion.choices[0].message.content)
+
+
+    elif inputs["promptingTechnique"] == "OP":
+        dataStore = open("{0}\ResultsOP\{1}\CWE-2023-{2}-{3}-{4}-testing.txt".format(inputs["model"], inputs["testingDataset"], inputs["testingCodeNumber"], inputs["promptingTechnique"], inputs["patchStatus"]), "x")
+        dataStore.write(chat_completion.choices[0].message.content)
 
     
     savePrompt(prompts[inputs["promptingTechnique"]], promptTemplate)
@@ -256,10 +256,10 @@ for testingDataset in dataset.keys():
 
         inputs["patchStatus"] = "patch"
         chatBot()
-        # time.sleep(20.0)
+        time.sleep(20.0)
 
         inputs["patchStatus"] = "vuln"
         chatBot()
-        # time.sleep(20.0)
+        time.sleep(20.0)
 
 print("\n\n\n------- ALL IS DONE! --------")
